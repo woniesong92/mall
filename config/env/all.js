942ad6829d68c5051722db0540a5127e9e2ea936
Howon Song <hs459@cornell.edu>
2015-01-04 17:23:59 +0900

    view and css update

--- config/env/all.js
+++ config/env/all.js
@@ -2,9 +2,9 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+		title: 'Miss Korea',
+		description: 'Number one shopping mall for cosmetics from Korea',
+		keywords: 'Korean, cosmetics, beauty, innisfree, goodal, asianbeauty'
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,6 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.title = 'MissKorea';
 	}
 ]);
